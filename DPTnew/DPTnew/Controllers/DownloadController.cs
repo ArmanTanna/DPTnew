858559fc8bc6c2e220b29b10d8ccd25a57795426
armantanna <arman.nazmul@solaircorporate.com>
2016-06-22 13:40:00 +0200

    hide 32bit product for japanese contact

--- DPTnew/DPTnew/Controllers/DownloadController.cs
+++ DPTnew/DPTnew/Controllers/DownloadController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using DPTnew.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace DPTnew.Controllers
 {
@@ -14,7 +16,14 @@ namespace DPTnew.Controllers
 
         public ActionResult Index()
         {
-            return View();
+            using (var db = new DptContext())
+            {
+                var user = Membership.GetUser().UserName;
+                var contact = db.Contacts.Where(u => u.Email == user).ToList().First();
+                var company = db.Companies.Where(u => u.AccountNumber == contact.AccountNumber).ToList().First();
+                ViewBag.Message = company.SalesRegion;
+                return View();
+            }
         }
 
     }
